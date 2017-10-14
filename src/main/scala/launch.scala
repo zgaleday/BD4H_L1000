ab414af186dd1d75c6296b834c96b70674438779
zgaleday <zgd3200@gmail.com>
2017-10-14 15:19:42 -0400

    Make SparkSession

--- src/main/scala/launch.scala
+++ src/main/scala/launch.scala
@@ -1,5 +1,11 @@
+import org.apache.spark.sql.SparkSession
+
 object launch {
   def main(args: Array[String]): Unit = {
     println("Time for some statistics")
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("L1000_data")
+      .getOrCreate()
   }
 }
