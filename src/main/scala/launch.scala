27f943645ee856196fbcc9332d9e7578690cd59e
zgaleday <zgd3200@gmail.com>
2017-10-15 18:35:37 -0400

    Commit launch changes.

--- src/main/scala/launch.scala
+++ src/main/scala/launch.scala
@@ -1,11 +1,15 @@
 import org.apache.spark.sql.SparkSession
+import l1000_feature_construc.l1000_feature_constr._
 
 object launch {
   def main(args: Array[String]): Unit = {
     println("Time for some statistics")
+
     val spark = SparkSession.builder()
       .master("local")
       .appName("L1000_data")
       .getOrCreate()
+
+    val l1000_data = loadFeatures(spark, "data/l1000_scala_features.txt")
   }
 }
