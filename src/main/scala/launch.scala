c9cac9e2baa7e58c9e2ffc6c0df55d1772d903ac
zgaleday <zgd3200@gmail.com>
2017-10-15 18:42:16 -0400

    Stage changes for merge

--- src/main/scala/launch.scala
+++ src/main/scala/launch.scala
@@ -5,11 +5,6 @@ object launch {
   def main(args: Array[String]): Unit = {
     println("Time for some statistics")
 
-    val spark = SparkSession.builder()
-      .master("local")
-      .appName("L1000_data")
-      .getOrCreate()
-
-    val l1000_data = loadFeatures(spark, "data/l1000_scala_features.txt")
+    val l1000_data = loadFeatures(ss, "data/l1000_scala_features.txt")
   }
 }
