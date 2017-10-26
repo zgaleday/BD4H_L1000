5551a31d982c0ff30405e7e7b5a39b1159c95af2
zgaleday <zgd3200@gmail.com>
2017-10-25 22:32:53 -0400

    Add pert info read in

--- src/main/scala/l1000_feature_construc/l1000_feature_constr.scala
+++ src/main/scala/l1000_feature_construc/l1000_feature_constr.scala
@@ -6,9 +6,18 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object l1000_feature_constr {
   def loadFeatures(spark: SparkSession): DataFrame = {
+    val perts = loadPerts(spark)
+    perts.show(10)
+    spark.read.format("csv").
+      option("header", "true").
+      option("delimiter", ",").
+      load("data/l1000_scala_features.txt")
+  }
+
+  def loadPerts(spark: SparkSession): DataFrame = {
     spark.read.format("csv").
       option("header", "true").
       option("delimiter", "\t").
-      load("data/l1000_scala_features.txt")
+      load("data/GSE92742_Broad_LINCS_sig_info.txt")
   }
 }
