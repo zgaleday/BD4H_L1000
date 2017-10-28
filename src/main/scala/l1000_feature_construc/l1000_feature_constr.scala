57efffd200b15a4f463be263e23929b6030c7fc0
zgaleday <zgd3200@gmail.com>
2017-10-28 11:59:17 -0400

    Comment code

--- src/main/scala/l1000_feature_construc/l1000_feature_constr.scala
+++ src/main/scala/l1000_feature_construc/l1000_feature_constr.scala
@@ -2,9 +2,12 @@ package l1000_feature_construc
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
 
 
 object l1000_feature_constr {
+
+  //Loads in the L1000 feature vectors with CIDs as the key
   def loadFeatures(spark: SparkSession): DataFrame = {
     val features = spark.read.format("csv").
       option("header", "true").
@@ -12,10 +15,11 @@ object l1000_feature_constr {
       load("data/l1000_scala_features.txt")
     val perts = loadPerts(spark).select("pert_id", "CID").
       join(features, "pert_id").drop("pert_id")
-    perts.show(10)
     perts
   }
 
+
+  //Loads in the perts from the output of CID curation (allowing for join with ADRs)
   def loadPerts(spark: SparkSession): DataFrame = {
     spark.read.format("csv").
       option("header", "true").
