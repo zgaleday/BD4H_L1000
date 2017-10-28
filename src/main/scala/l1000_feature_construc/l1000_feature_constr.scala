bb8ba92a5012edf7fb7ca1bdf0cad4f4893b32aa
zgaleday <zgd3200@gmail.com>
2017-10-28 11:43:30 -0400

    Transform feature vector to output CID instead of BRD#

--- src/main/scala/l1000_feature_construc/l1000_feature_constr.scala
+++ src/main/scala/l1000_feature_construc/l1000_feature_constr.scala
@@ -6,18 +6,20 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object l1000_feature_constr {
   def loadFeatures(spark: SparkSession): DataFrame = {
-    val perts = loadPerts(spark)
-    perts.show(10)
-    spark.read.format("csv").
+    val features = spark.read.format("csv").
       option("header", "true").
       option("delimiter", ",").
       load("data/l1000_scala_features.txt")
+    val perts = loadPerts(spark).select("pert_id", "CID").
+      join(features, "pert_id").drop("pert_id")
+    perts.show(10)
+    perts
   }
 
   def loadPerts(spark: SparkSession): DataFrame = {
     spark.read.format("csv").
       option("header", "true").
-      option("delimiter", "\t").
-      load("data/GSE92742_Broad_LINCS_sig_info.txt")
+      option("delimiter", ",").
+      load("data/perts_with_cid.txt")
   }
 }
