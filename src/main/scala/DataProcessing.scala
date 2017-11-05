09427d46ece90fa5ca1603a5e3a55f579d0b9362
zgaleday <zgd3200@gmail.com>
2017-11-05 12:02:39 -0500

    Add method to output file for classifications

--- src/main/scala/DataProcessing.scala
+++ src/main/scala/DataProcessing.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
 
 case class
 OffsidesDataRaw(stitchId: String, // 0
@@ -72,18 +73,16 @@ object DataProcessing {
 
     // first read in the offsides and SIDER data into RDDs
     val siderRDD: RDD[ADRdata] =
-        sc.textFile(sider_tsv)
-          .map(line =>
-            {
-                val cols = line.split("\t")
-                // ADRdata(convertSiderStitchId(cols(0)), cols(2), cols(5))
-                ADRdata(convertSiderStitchId(cols(0)), cols(2))
-            })
+      sc.textFile(sider_tsv)
+        .map(line => {
+          val cols = line.split("\t")
+          // ADRdata(convertSiderStitchId(cols(0)), cols(2), cols(5))
+          ADRdata(convertSiderStitchId(cols(0)), cols(2))
+        })
 
-    val offsidesRDD : RDD[ADRdata] =
+    val offsidesRDD: RDD[ADRdata] =
       sc.textFile(offsides_tsv)
-        .map(line =>
-        {
+        .map(line => {
           val cols = line.split("\t")
           //ADRdata(convertOffsidesStitchId(dropQuotes(cols(0))), dropQuotes(cols(2)), dropQuotes(cols(3)))
           ADRdata(convertOffsidesStitchId(dropQuotes(cols(0))), dropQuotes(cols(2)))
@@ -94,22 +93,20 @@ object DataProcessing {
     val adrRDD = siderRDD.union(offsidesRDD)
 
     // read in the L1000 ids
-    val l1000IdsRDD : RDD[L1000Ids] =
+    val l1000IdsRDD: RDD[L1000Ids] =
       sc.textFile(l1000ids_csv)
-        .map(line =>
-        {
+        .map(line => {
           val cols = line.split(",")
           L1000Ids(convertPertsStitchId(cols(3)), cols(1))
         })
 
     // read in the L1000 perts data
-    val l1000PertsRDD : RDD[L1000Perts] =
+    val l1000PertsRDD: RDD[L1000Perts] =
       sc.textFile(l1000perts_csv)
-          .map(line =>
-          {
-            val cols = line.split(",")
-            L1000Perts(cols(0), cols.slice(1,978).map(x => x.toDouble))
-          })
+        .map(line => {
+          val cols = line.split(",")
+          L1000Perts(cols(0), cols.slice(1, 978).map(x => x.toDouble))
+        })
 
     val drugs_sider = siderRDD.map(x => x.stitchId).distinct()
     val drugs_offsides = offsidesRDD.map(x => x.stitchId).distinct()
@@ -134,14 +131,50 @@ object DataProcessing {
     println("[info] Number of side effects = %d".format(se_adr.count()))
 
     val drug_se = adrRDD.map(x => (x.stitchId, x.umlsId)).groupByKey()
-    drug_se.coalesce(1, true).saveAsTextFile("all_side_effects")
+    //    drug_se.coalesce(1, true).saveAsTextFile("all_side_effects")
 
-    val drug_perts = l1000IdsRDD.map(x => (x.pertId, x.stitchId)).join(l1000PertsRDD.map(x =>(x.pertId, x.perts))).map(x => (x._2._1, x._2._2))
-    drug_perts.coalesce(1, true).saveAsTextFile("all_perts")
+    val drug_perts = l1000IdsRDD.map(x => (x.pertId, x.stitchId)).join(l1000PertsRDD.map(x => (x.pertId, x.perts))).map(x => (x._2._1, x._2._2))
+    //    drug_perts.coalesce(1, true).saveAsTextFile("all_perts")
 
     val drug_se_perts = drug_se.join(drug_perts)
-    println("[info Number of data rows = %d".format(drug_se_perts.count()) )
+    println("[info Number of data rows = %d".format(drug_se_perts.count()))
+
+    val adr_map = drug_se_perts.map(v => v._2._1.toSet).reduce(_ ++ _).zipWithIndex.toMap
+
+    def map_to_array(s: Set[String], a: Array[Int]): Array[Int] = {
+      if (s.isEmpty) {
+        a
+      }
+      else {
+        val index = adr_map.getOrElse(s.head, -1)
+        a(index) = 1
+        map_to_array(s.tail, a)
+      }
+    }
+
+    val output_df = drug_se_perts.map(v => (v._1, (v._2._1.toSet, Array.fill(adr_map.size) {
+      0
+    }), v._2._2.mkString(","))).
+      map(v => Array(v._1.toString, v._3, map_to_array(v._2._1, v._2._2).mkString(",")).mkString(","))
+
+    val reverse_map = adr_map.map(_.swap)
+    val range = Array.range(0, 1 + 978 + adr_map.size + 1).map(v => v.toString)
+    range(0) = "CID"
+
+    def build_header(a: Array[String], index: Int): Array[String] = {
+      if (index == a.length - 1) a
+      else if (a(index).toInt <= 978) build_header(a, index + 1)
+      else {
+        val adr = reverse_map.getOrElse(index - 978, "")
+        a(index) = adr
+        build_header(a, index + 1)
+      }
+    }
+
+    val header = sc.parallelize(List(build_header(range, 1).mkString(",")))
+    val output = header.union(output_df)
+
+    output.coalesce(1).saveAsTextFile("data/combined")
 
   }
-
 }
