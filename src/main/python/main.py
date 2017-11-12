91facc83024b34b4d3d03e5b688a7eab296a5be7
Deven Desai <deven.desai@gatech.edu>
2017-11-12 13:46:13 -0500

    fixing a bug where we were using the entire dataset to do training

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -6,6 +6,6 @@ x, y = get_train_test_data()
 target_distribution = compute_target_distribution(y, plot=False, verbose=False)
 
 
-#for model_type in ["logistic", "tree", "adaboost", "forest", "nnet"] :
-for model_type in ["forest"] :
-    multiclass_one_vs_rest(x, y, model_type, plot=False, verbose=True)
+#for model_type in ["logistic", "tree", "forest", "nnet", "adaboost"] :
+for model_type in ["nnet"] :
+    multiclass_one_vs_rest(x, y, model_type, plot=False, verbose=True, run_cv=False)
--- src/main/python/models.py
+++ src/main/python/models.py
@@ -38,7 +38,7 @@ def check_predictions(model, model_type, x, y, plot, verbose) :
 
     
 
-def multiclass_one_vs_rest(x, y, model_type='svm', plot=False, verbose=False):
+def multiclass_one_vs_rest(x, y, model_type='svm', plot=False, verbose=False, run_cv=False):
 
     # First split the data into training and test sets
     x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33, random_state=RANDOM_SEED)
@@ -47,23 +47,39 @@ def multiclass_one_vs_rest(x, y, model_type='svm', plot=False, verbose=False):
     # pick the base classifier based on the model_type paramemter
     if model_type is 'logistic':
         base_model = LogisticRegression(random_state=RANDOM_SEED, class_weight='balanced')
+
     elif model_type is 'tree':
         base_model = DecisionTreeClassifier(random_state=RANDOM_SEED, class_weight='balanced')
+
     elif model_type is 'adaboost':
         base_model = AdaBoostClassifier(random_state=RANDOM_SEED)
+
     elif model_type is 'forest':
+        # base case
         base_model = RandomForestClassifier(random_state=RANDOM_SEED, class_weight='balanced')
+
+        # this gives no improvement over base case
+        #base_model = RandomForestClassifier(random_state=RANDOM_SEED, class_weight='balanced', n_estimators = 50, max_depth=20)
+        
+        # no improvement here either
+        #base_model = RandomForestClassifier(random_state=RANDOM_SEED, class_weight='balanced', n_estimators = 100, max_depth=40)
+
     elif model_type is 'nnet':
         base_model = MLPClassifier(random_state=RANDOM_SEED)
+
     else:
+        #base case
         base_model = SVC(kernel='linear', random_state=RANDOM_SEED, class_weight='balanced')
+
+        # no improvement here 
+        #base_model = SVC(kernel='linear', random_state=RANDOM_SEED, class_weight='balanced', activation="logistic", max_iter=500)
 
     # create the OvR model using the base classifier
     model = OneVsRestClassifier(base_model, n_jobs=10)
 
     # train the model using the training data
     fit_start = time.time()
-    model.fit(x, y)
+    model.fit(x_train, y_train)
     fit_end = time.time()
 
     if verbose:
@@ -75,22 +91,23 @@ def multiclass_one_vs_rest(x, y, model_type='svm', plot=False, verbose=False):
     # check the accuracy on the training data
     if verbose:
         print('------ training data ----------')
-    check_predictions(model, model_type, x_train, y_train, plot, verbose)
+    check_predictions(model, (model_type + " - train"), x_train, y_train, plot, verbose)
     
     # check the accuracy on the test data
     if verbose:
         print('------ test data ----------')
-    check_predictions(model, model_type, x_test, y_test, plot, verbose)
+    check_predictions(model, (model_type + " - test"), x_test, y_test, plot, verbose)
 
     # get the cross-validation score
-
-    accuracy_scorer = make_scorer(calculate_overall_accuracy)
-    cv_accuracy_scores = cross_val_score(model, x, y, scoring=accuracy_scorer, cv=5)
-    cv_auc_scores = cross_val_score(model, x, y, scoring="roc_auc", cv=5)
+    if run_cv :
+        
+        accuracy_scorer = make_scorer(calculate_overall_accuracy)
+        cv_accuracy_scores = cross_val_score(model, x, y, scoring=accuracy_scorer, cv=5)
+        cv_auc_scores = cross_val_score(model, x, y, scoring="roc_auc", cv=5)
     
-    if verbose:
-        print('------ CV scores ----------')
-        print('one vs all ' + model_type + ' CV accuracy scores' + str(cv_accuracy_scores))
-        print('one vs all ' + model_type + ' CV AUC scores' + str(cv_auc_scores))
+        if verbose:
+            print('------ CV scores ----------')
+            print('one vs all ' + model_type + ' CV accuracy scores ' + str(cv_accuracy_scores))
+            print('one vs all ' + model_type + ' CV AUC scores ' + str(cv_auc_scores))
     
     return 
