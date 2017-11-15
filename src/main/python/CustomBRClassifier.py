4f48a9c9d06b3a0165269d2c10d5387943845df7
zgaleday <zgd3200@gmail.com>
2017-11-14 21:13:39 -0500

    Add predict and predict prob methods to CustomBRClassifier

--- src/main/python/CustomBRClassifier.py
+++ src/main/python/CustomBRClassifier.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
+import array
 from sklearn.base import BaseEstimator, ClassifierMixin, clone, is_classifier
-from sklearn.utils.validation import  check_is_fitted
+from sklearn.base import MetaEstimatorMixin, is_regressor
+from sklearn.utils.validation import check_is_fitted, _num_samples
+import scipy.sparse as sp
 from sklearn.feature_selection import *
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from utils import get_train_test_data
 from sklearn.preprocessing import LabelBinarizer
-import pickle
 from joblib import Parallel, delayed
 from ipyparallel import *
 
@@ -51,6 +53,17 @@ class _ConstantPredictor(BaseEstimator):
 
         return np.repeat([np.hstack([1 - self.y_, self.y_])], X.shape[0], axis=0)
 
+def _predict_binary(estimator, X):
+    """Make predictions using a single binary estimator."""
+    if is_regressor(estimator):
+        return estimator.predict(X)
+    try:
+        score = np.ravel(estimator.decision_function(X))
+    except (AttributeError, NotImplementedError):
+        # probabilities of the positive class
+        score = estimator.predict_proba(X)[:, 1]
+    return score
+
 
 RANDOM_SEED = 0
 
@@ -85,7 +98,7 @@ class CustomBRClassifier(object):
         # register_parallel_backend('ipyparallel', lambda: IPythonParallelBackend(view=bview))
         if select_feats:
             lr = LogisticRegression(penalty='l1')
-            selector = RFE(lr, 25, step=978, verbose=1)
+            selector = RFE(lr, 25, step=10, verbose=1)
             ovr_fs = OneVsRestClassifier(selector, n_jobs=-1)
             ovr_fs.fit(self.X, self.Y)
             self.selectors = ovr_fs.estimators_
@@ -118,13 +131,89 @@ class CustomBRClassifier(object):
         selector = SelectFromModel(estimator=LogisticRegression(penalty='l1', random_state=RANDOM_SEED)
                     )
         classes = self.Y[:, index]
-        print index
         return selector.fit_transform(self.X, classes)
+
+    def predict(self, X):
+        """Predict multi-class targets using underlying estimators.
+        Parameters
+        ----------
+        X : (sparse) array-like, shape = [n_samples, n_features]
+            Data.
+        Returns
+        -------
+        y : (sparse) array-like, shape = [n_samples, ], [n_samples, n_classes].
+            Predicted multi-class targets.
+        """
+        check_is_fitted(self, 'estimators_')
+        if (hasattr(self.estimators_[0], "decision_function") and
+                is_classifier(self.estimators_[0])):
+            thresh = 0
+        else:
+            thresh = .5
+
+        n_samples = _num_samples(X)
+        if self.label_binarizer_.y_type_ == "multiclass":
+            maxima = np.empty(n_samples, dtype=float)
+            maxima.fill(-np.inf)
+            argmaxima = np.zeros(n_samples, dtype=int)
+            for i, e in enumerate(self.estimators_):
+                pred = _predict_binary(e, X)
+                np.maximum(maxima, pred, out=maxima)
+                argmaxima[maxima == pred] = i
+            return self.classes_[np.array(argmaxima.T)]
+        else:
+            indices = array.array('i')
+            indptr = array.array('i', [0])
+            for e in self.estimators_:
+                indices.extend(np.where(_predict_binary(e, X) > thresh)[0])
+                indptr.append(len(indices))
+            data = np.ones(len(indices), dtype=int)
+            indicator = sp.csc_matrix((data, indices, indptr),
+                                      shape=(n_samples, len(self.estimators_)))
+            return self.label_binarizer_.inverse_transform(indicator)
+
+    def predict_proba(self, X):
+        """Probability estimates.
+        The returned estimates for all classes are ordered by label of classes.
+        Note that in the multilabel case, each sample can have any number of
+        labels. This returns the marginal probability that the given sample has
+        the label in question. For example, it is entirely consistent that two
+        labels both have a 90% probability of applying to a given sample.
+        In the single label multiclass case, the rows of the returned matrix
+        sum to 1.
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+        Returns
+        -------
+        T : (sparse) array-like, shape = [n_samples, n_classes]
+            Returns the probability of the sample for each class in the model,
+            where classes are ordered as they are in `self.classes_`.
+        """
+        check_is_fitted(self, 'estimators_')
+        # Y[i, j] gives the probability that sample i has the label j.
+        # In the multi-label case, these are not disjoint.
+        Y = np.array([e.predict_proba(X)[:, 1] for e in self.estimators_]).T
+
+        if len(self.estimators_) == 1:
+            # Only one estimator, but we still want to return probabilities
+            # for two classes.
+            Y = np.concatenate(((1 - Y), Y), axis=1)
+
+        if not self.multilabel_:
+            # Then, probabilities should be normalized to 1.
+            Y /= np.sum(Y, axis=1)[:, np.newaxis]
+        return Y
 
     def _load_features(self):
         self.new_feats = np.load("../../../data/selected_features.npy")
 
+    def multilabel_(self):
+        """Whether this is a multilabel classifier"""
+        return self.label_binarizer_.y_type_.startswith('multilabel')
+
+
 x, y = get_train_test_data()
 base = LogisticRegression(penalty='l1', random_state=RANDOM_SEED)
 br = CustomBRClassifier(base, 25)
-br.fit(x,y, False)
+br.fit(x,y, True)
