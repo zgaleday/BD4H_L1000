24ebc5ea4989dadf7058f7cf90f6d4af2a1983e7
zgaleday <zgd3200@gmail.com>
2017-12-01 21:42:01 -0500

    Add histogram plotting methods for ROC and AUC of each estimator

--- src/main/python/plotting.py
+++ src/main/python/plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
+from sklearn.metrics import roc_auc_score
 
 def plot_hist(y, title):
     plt.hist(y)
@@ -60,4 +61,32 @@ def plot_combined_roc_curve(plot_data):
     plt.show()
 
 
-    
+def generate_accuracy_hist(y_test, predictions):
+    """
+    Read in the testing labels and the predicted labels on the test data and produced a histogram of accuracies (defined
+    by assigning a higher probability to the correct label)
+    :param y_test: the true labels on the test data used to generate predictions
+    :param predictions: the predicted labels generate from the training data
+    """
+    hard_labels = np.round(predictions)
+    diff_matrix = np.abs(y_test - hard_labels)
+    perlabel_accuacy = 1 - np.mean(diff_matrix, axis=0)
+    plt.hist(perlabel_accuacy, bins=7)
+    plt.title("Accuracy of estimators")
+    plt.show()
+
+
+def generate_roc_hist(y_test, predictions):
+    """
+    See generate_accuracy hist only difference is the plot show AUCROC as opposed to accuracy.
+    """
+    roc_scores = []
+    for col in range(y_test.shape[1]):
+        y_col = y_test[:, col]
+        pred_col = predictions[:, col]
+        score = roc_auc_score(y_col, pred_col)
+        roc_scores.append(score)
+    roc_scores = np.array(roc_scores)
+    plt.hist(roc_scores, bins=7, edgecolor='black')
+    plt.title("ROCAUC of estimators")
+    plt.show()
